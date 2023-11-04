eae14f6a3b69d8034261074465b868b5c569c579
felipe-ra <felipeangel50@gmail.com>
2023-11-04 16:17:25 -0500

    added question 4 answers with bonus challenge

--- part1/question4.py
+++ part1/question4.py
@@ -1,5 +1,5 @@
 import pets_db
-
+import sqlite3
 ################################################################################
 #     ____                          __     _                          __ __
 #    / __ \  __  __  ___    _____  / /_   (_)  ____    ____          / // /
@@ -22,27 +22,49 @@ import pets_db
 # Write SQL to select the pets that are owned by nobody.
 # The output should be a list of tuples in the format: (<pet name>, <species>, <age>)
 
+
 sql_pets_owned_by_nobody = """
 
-Your SQL here.
+SELECT ANIMALS.name, ANIMALS.species, ANIMALS.age
+FROM ANIMALS
+LEFT JOIN PEOPLE_ANIMALS ON ANIMALS.animal_id = PEOPLE_ANIMALS.pet_id
+WHERE PEOPLE_ANIMALS.owner_id IS NULL;
 
 """
+                        
 
 # Part 4.B:
-# Write SQL to select how the number of pets are older than their owners. 
+# Write SQL to select  the number of pets that are older than their owners. 
 # The output should be an integer.
 
 sql_pets_older_than_owner = """
 
-Your SQL here.
+SELECT SUM(CASE WHEN ANIMALS.age > PEOPLE.age THEN 1 ELSE 0 END) AS count
+FROM ANIMALS
+INNER JOIN PEOPLE_ANIMALS ON ANIMALS.animal_id = PEOPLE_ANIMALS.pet_id
+INNER JOIN PEOPLE ON PEOPLE_ANIMALS.owner_id = PEOPLE.person_id;
 
 """
 
 # Part 4.C: BONUS CHALLENGE! 
 # Write SQL to select the pets that are owned by Bessie and nobody else.
 # The output should be a list of tuples in the format: (<person name>, <pet name>, <species>)
+
+
+# This query aims to be as efficient as possible by limiting subqueries usage 
+# We used an existential subquery to facilitate the conditional filtering
 sql_only_owned_by_bessie = """ 
 
-Your SQL here.
+SELECT P.name AS person_name, A.name AS pet_name, A.species
+FROM ANIMALS A
+JOIN PEOPLE_ANIMALS PA ON A.animal_id = PA.pet_id
+JOIN PEOPLE P ON PA.owner_id = P.person_id
+WHERE LOWER(P.name) = 'bessie'
+AND NOT EXISTS (
+    SELECT 1
+    FROM PEOPLE_ANIMALS PA2
+    WHERE PA2.pet_id = PA.pet_id
+    AND PA2.owner_id <> P.person_id
+);
 
 """
