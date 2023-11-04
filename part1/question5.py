24a4f0c43e0653480c5c1e27629d757dcc87b26c
felipe-ra <felipeangel50@gmail.com>
2023-11-04 16:53:18 -0500

    added solutions to question5

--- part1/question5.py
+++ part1/question5.py
@@ -20,7 +20,12 @@
 
 sql_create_favorite_foods = """
 
-Your SQL here.
+CREATE TABLE FAVORITE_FOODS (
+    food_id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    vegetarian INTEGER NOT NULL CHECK (vegetarian IN (0, 1))
+);
+
 
 """
 
@@ -28,9 +33,42 @@ Your SQL here.
 # Alter the animals and people tables by adding a new column to each called 'favorite_food_id'
 # The test suite will verify the new changes by inserting some new rows. 
 
+
 sql_alter_tables_with_favorite_food = """
 
-Your SQL here.
+ALTER TABLE ANIMALS
+ADD COLUMN favorite_food_id INTEGER;
+
+ALTER TABLE PEOPLE
+ADD COLUMN favorite_food_id INTEGER;
+
+
+/*
+    The FK allows us to do faster queries (since they are usually a prime target for indexing)
+    and to enforce the business model
+
+    SADLY, sqlite does not allow to ADD CONSTRAINT while doing an ALTER TABLE operation.
+
+    Constrains are only available during table creation (IN SQLITE, other engines allow it during ALTER TABLE Ops)
+    
+    For reference, here the instructions to do FK on ANIMALS and PEOPLE:
+
+    -- Create a foreign key constraint on 'favorite_food_id' in the 'ANIMALS' table
+    ALTER TABLE ANIMALS
+    ADD CONSTRAINT fk_animals_favorite_food
+    FOREIGN KEY (favorite_food_id)
+    REFERENCES FAVORITE_FOODS(food_id);
+
+    -- Create a foreign key constraint on 'favorite_food_id' in the 'PEOPLE' table
+    ALTER TABLE PEOPLE
+    ADD CONSTRAINT fk_people_favorite_food
+    FOREIGN KEY (favorite_food_id)
+    REFERENCES FAVORITE_FOODS(food_id);
+
+
+*/
+
+
 
 """
 
@@ -40,6 +78,9 @@ Your SQL here.
 
 sql_select_all_vegetarian_pets = """
 
-Your SQL here.
+SELECT A.name AS pet_name, F.name AS food_name
+FROM ANIMALS A
+JOIN FAVORITE_FOODS F ON A.favorite_food_id = F.food_id
+WHERE F.vegetarian = 1;
 
 """
