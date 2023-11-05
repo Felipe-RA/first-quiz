e00a1bd030df4bf2751fbcf0852000f003a2923e
felipe-ra <felipeangel50@gmail.com>
2023-11-04 19:53:46 -0500

    added Singleton impl of VendingMachine

--- part2/src/main/java/org/velezreyes/quiz/question6/VendingMachineImpl.java
+++ part2/src/main/java/org/velezreyes/quiz/question6/VendingMachineImpl.java
@@ -1,9 +1,44 @@
 package org.velezreyes.quiz.question6;
 
-public class VendingMachineImpl {
+public class VendingMachineImpl implements VendingMachine {
 
-  public static VendingMachine getInstance() {
-    // Fix me!
-    return null;
-  }
+    private int insertedMoneyCents = 0;
+    private static final int QUARTER_VALUE_CENTS = 25;
+
+    private static VendingMachineImpl instance = new VendingMachineImpl();
+
+    private VendingMachineImpl() {
+        // Private constructor for singleton
+    }
+
+    public static VendingMachine getInstance() {
+        return instance;
+    }
+
+    @Override
+    public void insertQuarter() {
+        insertedMoneyCents += QUARTER_VALUE_CENTS; // Add 25 cents to the inserted money
+    }
+
+    @Override
+    public Drink pressButton(String name) throws NotEnoughMoneyException, UnknownDrinkException {
+        int price = DrinkFactory.getPrice(name);
+        if (insertedMoneyCents < price) {
+            throw new NotEnoughMoneyException("Inserted money is not enough for " + name + ".");
+        }
+
+        // Subtract the price of the drink from the inserted money
+        insertedMoneyCents -= price;
+        return DrinkFactory.getDrink(name);
+    }
+
+    // Additional method to reset the machine for the next user
+    public void reset() {
+        this.insertedMoneyCents = 0;
+    }
+
+    // Additional method to get the current amount of money inserted
+    public int getMoneyInserted() {
+        return this.insertedMoneyCents;
+    }
 }
