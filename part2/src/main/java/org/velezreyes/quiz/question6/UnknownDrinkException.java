82f0455ec9751f40d815d6ae90599b37bbc426fd
felipe-ra <felipeangel50@gmail.com>
2023-11-04 19:05:33 -0500

    added customizable error message for exception

--- part2/src/main/java/org/velezreyes/quiz/question6/UnknownDrinkException.java
+++ part2/src/main/java/org/velezreyes/quiz/question6/UnknownDrinkException.java
@@ -5,4 +5,8 @@ public class UnknownDrinkException extends Exception {
   public UnknownDrinkException() {
     super("Unknown drink!");
   }
+
+  public UnknownDrinkException(String message) {
+    super(message);
+  }
 }
